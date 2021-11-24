21bc86cd9dd7e201908375d50f85013c199e177f
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2021-11-24 15:05:00 +0100

    working version

--- 1.3_get_next_line/main.c
+++ 1.3_get_next_line/main.c
@@ -14,12 +14,6 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-#define MAIN(string)		"\033[1m\033[38;5;199m" string "\033[0m"
-#define MAIN_1(string)		"\033[35m" string "\033[0m"
-#define HEADER_1(string)	"\033[38;5;43m" string "\033[0m"
-#define HEADER_2(string)	"\033[38;5;75m" string "\033[0m"
-#define HIGHLIGHT(string)	"\033[38;5;191m" string "\033[0m"
-
 void	test(int fd)
 {
 	char	*line;
@@ -34,42 +28,16 @@ void	test(int fd)
 
 int	main(void)
 {
-	printf(MAIN("=== • Program started • ===\n"));
-	printf(MAIN_1("=== empty file ===\n"));
+	printf("--- hello ---\n");
 	int	fda;
-	fda = open("txt_files/t_empty.txt", O_RDONLY);
+	fda = open("txt_files/hello.txt", O_RDONLY);
 	test(fda);
-	printf(MAIN_1("=== 5 nl ===\n"));
+	printf("--- nbr ---\n");
 	int	fdf;
-	fdf = open("txt_files/5nl.txt", O_RDONLY);
+	fdf = open("txt_files/nbr.txt", O_RDONLY);
 	test(fdf);
-	printf(MAIN_1("=== \\n file ===\n"));
+	printf("--- nbr2 ---\n");
 	int	fdb;
-	fdb = open("txt_files/t_n.txt", O_RDONLY);
+	fdb = open("txt_files/nbr2.txt", O_RDONLY);
 	test(fdb);
-	printf(MAIN_1("=== 43 NL ===\n"));
-	int	fdi;
-	fdi = open("txt_files/43_nl.txt", O_RDONLY);
-	test(fdi);
-	printf(MAIN_1("=== 43 WITH \n ===\n"));
-	int	fdj;
-	fdj = open("txt_files/43_wl.txt", O_RDONLY);
-	test(fdj);
-	printf(MAIN_1("=== Alternate line and nl ===\n"));
-	int	fde;
-	fde = open("txt_files/alt_ln.txt", O_RDONLY);
-	test(fde);
-	printf(MAIN_1("=== BIG LINE NO NL ===\n"));
-	int	fdg;
-	fdg = open("txt_files/bl_no.txt", O_RDONLY);
-	test(fdg);
-	printf(MAIN_1("=== BIG LINE WITH NL ===\n"));
-	int	fdh;
-	fdh = open("txt_files/bl_wh.txt", O_RDONLY);
-	test(fdh);
-	printf(MAIN_1("=== Le Petit Prince ===\n"));
-	int	fdc;
-	fdc = open("txt_files/t_ptiprinc.txt", O_RDONLY);
-	test(fdc);
-	printf(MAIN("=== • Program ended • ===\n\n"));
 }
