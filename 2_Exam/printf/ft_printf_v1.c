ea285b944ceed2b13172a88d62c7b2e4dc17e236
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2021-11-29 16:30:42 +0100

    print f progress

--- 2_Exam/printf/ft_printf_v1.c
+++ 2_Exam/printf/ft_printf_v1.c
@@ -6,9 +6,11 @@
 /*   By: mmeersma <mmeersma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:16:23 by mmeersma          #+#    #+#             */
-/*   Updated: 2021/11/29 15:44:41 by mmeersma         ###   ########.fr       */
+/*   Updated: 2021/11/29 16:29:54 by mmeersma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// s d x
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -16,19 +18,126 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int ft_handle_s(char *str) // write char and return amount of chars
+{
+	int i;
+	
+	i = 0;
+	if (!str)
+		str = ("(null)");
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+		i++;
+	}
+	return (i);
+}
+
+int ft_handle_d(int num)
+{
+	char *str;
+	int i;
+	
+	i = 0;
+	str = ft_itoa(num, 10);
+	i = ft_handle_s(str);
+	free(str);
+	return (i);
+}
+
+int ft_handle_x(unsigned int num)
+{
+	char *str;
+	int i;
+	
+	i = 0;
+	str = ft_itoa_base(num, 16);
+	str = ft_str_tolower(str);
+	i = ft_handle_s(str);
+	free(str);
+	return (i);
+}
+
+int ft_putchar_count(char c) // write char and return 1
+{
+	write(1, &c, 1); 
+	return (1);
+}
+
+int ft_strlen(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_strdup(const char *s)
+{
+	char *ret;
+	int i;
+	int len;
+
+	i = 0;											// set zero
+	len = ft_strlen(s);								// get the length of the string
+	ret = (char *)malloc((len + 1) * sizeof(char));	// allocate space to the returning string
+	if (ret == NULL)								// if allocating didnt work, return zero
+		return (NULL);
+	while (s[i])									// go over the original string
+	{
+		ret[i] = s[i];								// copy the characters
+		i++;										// go to the next position
+	}
+	ret[i] = '\0';									// add the ending character
+	return (ret);									// return the duplicate string
+}
+
+int ft_is_conv(char c)
+{
+	return (c == 's' || c == 'd' || c == 'x');
+}
+
+int ft_handle_conv(int type, va_list args)
+{
+	int i;
+
+	i = 0;
+	if(type == 's')									// is string
+		i = ft_handle_s(va_arg(args, char *));
+	else if(type == 'd')							// is digit
+		i = ft_handle_d(va_arg(args, int));
+	else if(type == 'x')							// is hexadecimal
+		i = ft_handle_x(va_arg(args, unsigned int));
+	return (i);
+}
+
 int ft_handle_input(const char *format, va_list args)
 {
 	int type;
 	int i;
 	int char_count;
 
-	i = 0;												// setting vars to 0
+	i = 0;													// setting vars to 0
 	char_count = 0;
-	while (format[i])									// go over string
+	while (format[i])										// go over string
 	{
-		if (format[i] != '%')							// if it's just a character, just print it and add to the count
+		if (format[i] != '%')								// if it's just a character, just print it and add to the count
 			char_count += ft_putchar_count(format[i]);
-		else if (format[i] == '%' && format)
+		else if (format[i] == '%' && format[i + 1])			// if it is the modifier and there is a char behind it
+		{
+			i++;											// add one for the %
+			type = format[i];								// get the type
+			if (ft_is_conv(format[i]))						// is it a modifier
+				char_count += ft_handle_conv(type, args);	// handle the conversion
+			else
+				char_count += ft_putchar_count(format[i]);	// just put the character
+		}
+		i++;
 	}
 	
 }
@@ -51,7 +160,9 @@ int ft_printf(const char *format, ... )
 //*
 int main ()
 {
-	ft_printf("test");
+	ft_printf("%d", 5);
+	ft_printf("%s", "hello");
+	ft_printf("%x", 8);
 	return (0);
 }
 //*/
