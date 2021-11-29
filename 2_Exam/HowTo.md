a72678ad151c1a53aa6945be0178c36c26ce32b6
Mfficiency <63962741+Mfficiency@users.noreply.github.com>
2021-11-29 13:51:03 +0100

    exam rank2 prep

---
+++ 2_Exam/HowTo.md
@@ -0,0 +1,11 @@
+# RANK 2
+How to prepare for the exam of rank 2
+
+exam exists of 2 parts
+
+1. iter or union
+2. printf or get_next_line
+
+links:
+https://github.com/Glagan/42-exam-rank-02 
+
---
+++ 2_Exam/ft_iter_example.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iter.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmeersma <mmeersma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 13:47:43 by mmeersma          #+#    #+#             */
+/*   Updated: 2021/11/29 13:48:27 by mmeersma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	used[255];
+	int	i, j;
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (i < 255)
+			used[i++] = 0;
+		i = 2;
+		while (i > 0)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (i == 2 && !used[(unsigned char)argv[i][j]])
+					used[(unsigned char)argv[i][j]] = 1;
+				else if (i == 1 && used[(unsigned char)argv[i][j]] == 1)
+				{
+					write(1, &argv[i][j], 1);
+					used[(unsigned char)argv[i][j]] = 2;
+				}
+				j++;
+			}
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
